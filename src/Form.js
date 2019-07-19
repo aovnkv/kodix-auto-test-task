3c276ae0ad32db5c3ba715054aa1e866eba32d71
Andrei Vaniukov <aovnkv@yahoo.com>
2019-07-19 13:59:01 +0300

    style: form

--- src/Form.js
+++ src/Form.js
@@ -5,7 +5,7 @@ class Form extends Component {
   render() {
     return (
       <form className="newCar--form">
-        <div className="newCar--form-row1">
+        <div className="newCar--form--row1">
           <div className="newCar--form-title">
             <input name="title" type="text" id="_title" />
             <label htmlFor="_title">Название</label>
@@ -20,44 +20,59 @@ class Form extends Component {
           </div>
         </div>
 
-        <div className="newCar--form-row2">
+        <div className="newCar--form--row2">
           <div className="newCar--form-compl">
             <input name="title" type="text" id="_compl" />
             <label htmlFor="_compl">Описание</label>
           </div>
         </div>
 
-        <div className="newCar--form-row3">
+        <div className="newCar--form--row3">
           <fieldset>
             <legend>Цвет</legend>
-            <div className="newCar--form-colors">
-              <input type="radio" id="newCar-color--white" name="white" />
-              <label htmlFor="newCar-color--white">Белый</label>
+            <div className="newCar--form--color">
+              <input type="radio" id="_white" name="white" value="white" />
+              <label className="newCar--form--color-white" htmlFor="_white">
+                Белый
+              </label>
 
-              <input type="radio" id="newCar-color--black" name="black" />
-              <label htmlFor="newCar-color--black">Черный</label>
+              <input type="radio" id="_black" name="black" value="black" />
+              <label className="newCar--form--color-black" htmlFor="_black">
+                Черный
+              </label>
 
-              <input type="radio" id="newCar-color--gray" name="gray" />
-              <label htmlFor="newCar-color--gray">Серый</label>
+              <input type="radio" id="_gray" name="gray" value="gray" />
+              <label className="newCar--form--color-gray" htmlFor="_gray">
+                Серый
+              </label>
 
-              <input type="radio" id="newCar-color--red" name="red" />
-              <label htmlFor="newCar-color--red">Красный</label>
+              <input type="radio" id="_red" name="red" value="red" />
+              <label className="newCar--form--color-red" htmlFor="_red">
+                Красный
+              </label>
 
-              <input type="radio" id="newCar-color--green" name="green" />
-              <label htmlFor="newCar-color--green">Зеленый</label>
+              <input type="radio" id="_green" name="green" value="green" />
+              <label className="newCar--form--color-green" htmlFor="_green">
+                Зеленый
+              </label>
             </div>
           </fieldset>
 
-          <label htmlFor="newCar-status">Статус</label>
-          <select id="newCar-status">
-            <option value="">Статус</option>
-            <option value="dog">В наличии</option>
-            <option value="cat">Ожидается</option>
-            <option value="hamster">Нет в наличии</option>
-          </select>
+          <div className="newCar--form-status">
+            <select id="_status">
+              <option value="">Статус</option>
+              <option value="dog">В наличии</option>
+              <option value="cat">Ожидается</option>
+              <option value="hamster">Нет в наличии</option>
+            </select>
+            <label htmlFor="_status">Статус</label>
+          </div>
 
-          <label htmlFor="newCar-submit">Отправить</label>
-          <input type="submit" value="Отправить" />
+          <button
+            className="newCar--form-submit"
+            type="submit"
+            name="newCar--form-submit"
+            value="submit"><span className="newCar--form-submit-text">Отправить</span></button>
         </div>
       </form>
     );
