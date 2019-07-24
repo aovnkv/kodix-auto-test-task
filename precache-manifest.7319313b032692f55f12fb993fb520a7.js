35940515da99524b5497737978a67c2f8bd08e22
Andrei Vaniukov <aovnkv@yahoo.com>
2019-07-24 13:08:07 +0300

    Updates

---
+++ precache-manifest.7319313b032692f55f12fb993fb520a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e0c11488e79ba6a4bb581cd9f6644c",
+    "url": "/kodix-ultimate-test/index.html"
+  },
+  {
+    "revision": "576601e40897b677fd96",
+    "url": "/kodix-ultimate-test/static/css/main.e973c8de.chunk.css"
+  },
+  {
+    "revision": "acf56ea5c3c22e2cbd5f",
+    "url": "/kodix-ultimate-test/static/js/2.bc1f5e88.chunk.js"
+  },
+  {
+    "revision": "576601e40897b677fd96",
+    "url": "/kodix-ultimate-test/static/js/main.770efd8c.chunk.js"
+  },
+  {
+    "revision": "d7da7b91eb5f81fdaa7d",
+    "url": "/kodix-ultimate-test/static/js/runtime~main.8e67689b.js"
+  },
+  {
+    "revision": "97be63353a61d939bdb8147933ba1c61",
+    "url": "/kodix-ultimate-test/static/media/PK&co.97be6335.woff"
+  },
+  {
+    "revision": "6ae214a2d59ffcf2eaa7bd44e0bdb171",
+    "url": "/kodix-ultimate-test/static/media/arrow-small-right.6ae214a2.svg"
+  },
+  {
+    "revision": "ad2bc8d3b3896331e0de5b65b8e93259",
+    "url": "/kodix-ultimate-test/static/media/duck.ad2bc8d3.png"
+  },
+  {
+    "revision": "19166b1f4c84995dc0ab44d02009cf89",
+    "url": "/kodix-ultimate-test/static/media/duck@2x.19166b1f.png"
+  },
+  {
+    "revision": "f5a8286c54dcdb5cfc5997af5a265bdb",
+    "url": "/kodix-ultimate-test/static/media/duck@3x.f5a8286c.png"
+  },
+  {
+    "revision": "64e2a3f34916d90bc397f98b0138aa09",
+    "url": "/kodix-ultimate-test/static/media/rectangle-1.64e2a3f3.svg"
+  }
+]);
